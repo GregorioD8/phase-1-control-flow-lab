00126883279ab260840249cb336c88cfc1f5fda8
Gregorio <Greg.del801@gmail.com>
2023-06-27 18:51:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  
+  // gives customers a free sample if the ride is less than or equal to 400 feet
+  if (ride <= 400) {
+    return 'This one is on me!'
+  }
+  
+  // charges 20 dollars for a distance between 400 and 2000 feet
+  if (ride > 400 && ride < 2000) {
+    return 'That will be twenty bucks.'
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  // charges 30 dollars for a distance over 2000 feet
+  if (ride > 2000 && ride < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride && ride > 2500) {
+      return 'No can do.'
+    }
+      
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+// returns "Ok, sounds good." when the city is NYC
+function ternaryCheckCity(city){
+  
+  return (city === 'NYC'? 'Ok, sounds good.' : 'No go.')
+}
+  
+//return "Thank you so much." if the tip is generous
+//returns "Thank you." if the tip is not as generous
+//returns "Bye." if anything else
+function switchOnCharmFromTip(tip){
+
+ switch (tip) {
+  case 'generous':
+    return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.'
+  default:
+    return 'Bye.'
+
+ }
 }
